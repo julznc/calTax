b662d22d431b14e81dc3ac85159ac07eca58fa99
meann <mzabanal@bitbucket.org>
2015-06-14 18:13:02 +0800

    Add options for pay period

--- app/src/main/java/com/ymsoftlabs/caltax/CalTaxActivity.java
+++ app/src/main/java/com/ymsoftlabs/caltax/CalTaxActivity.java
@@ -2,16 +2,30 @@ package com.ymsoftlabs.caltax;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.AdapterView.OnItemSelectedListener;
 
 
-public class CalTaxActivity extends ActionBarActivity {
+public class CalTaxActivity extends ActionBarActivity implements  OnItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cal_tax);
+
+        Spinner spinner = (Spinner) findViewById(R.id.payChoices);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.basicPay_array, android.R.layout.simple_spinner_item);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
     }
 
     @Override
@@ -34,5 +48,13 @@ public class CalTaxActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+        Object result = parent.getItemAtPosition(pos);
+    }
+
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
