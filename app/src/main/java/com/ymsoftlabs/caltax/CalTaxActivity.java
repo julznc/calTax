133f26522ac9eb667708461940c72dcead1e2da2
meann <mzabanal@bitbucket.org>
2015-06-15 12:56:32 +0800

    Initialize objects and listeners

--- app/src/main/java/com/ymsoftlabs/caltax/CalTaxActivity.java
+++ app/src/main/java/com/ymsoftlabs/caltax/CalTaxActivity.java
@@ -2,16 +2,40 @@ package com.ymsoftlabs.caltax;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.AdapterView.OnItemSelectedListener;
 
 public class CalTaxActivity extends ActionBarActivity implements  OnItemSelectedListener {
+
+    int paymentPeriod = 0;
+    int employment = 0;
+    int civilStatus = 0;
+    double salary = 0;
+
+    EditText sssEditTextView;
+    EditText phEditTextView;
+    EditText pagibigEditTextView;
+
+    EditText salaryEditTextView;
+    EditText taxAllowanceTextView;
+    EditText otPayEditTextView;
+    EditText nightDiffEditTextView;
+    EditText holidayEditTextView;
+
+    EditText hmoEditTextView;
+    EditText absencesTextView;
+    EditText tardinessTextView;
+    EditText undertimeEditTextView;
+    EditText shieldedEditTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,8 +69,39 @@ public class CalTaxActivity extends ActionBarActivity implements  OnItemSelected
         status.setAdapter(statusAdapter);
         status.setOnItemSelectedListener(this);
 
-        ContributionsManager contributionsManager = new ContributionsManager();
-        contributionsManager.philhealthContribution(23999.99, 1);
+        sssEditTextView = (EditText) findViewById(R.id.sssContribution);
+        phEditTextView = (EditText) findViewById(R.id.philHealth);
+        pagibigEditTextView = (EditText) findViewById(R.id.pagibig);
+
+        salaryEditTextView = (EditText)findViewById(R.id.basicSalary);
+        taxAllowanceTextView = (EditText)findViewById(R.id.allowance);
+        otPayEditTextView = (EditText) findViewById(R.id.overtime);
+        nightDiffEditTextView = (EditText) findViewById(R.id.nightDiff);
+        holidayEditTextView = (EditText) findViewById(R.id.holiday);
+
+        hmoEditTextView = (EditText)findViewById(R.id.hmo);
+        absencesTextView = (EditText)findViewById(R.id.absences);
+        tardinessTextView = (EditText) findViewById(R.id.tardiness);
+        undertimeEditTextView = (EditText) findViewById(R.id.undertime);
+        shieldedEditTextView = (EditText) findViewById(R.id.shielded);
+
+        TextWatcher watch = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        };
+        salaryEditTextView.addTextChangedListener(watch);
     }
 
     @Override
@@ -72,10 +127,44 @@ public class CalTaxActivity extends ActionBarActivity implements  OnItemSelected
     }
 
     public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
-        Object result = parent.getItemAtPosition(pos);
+        String result = parent.getItemAtPosition(pos).toString();
+
+        if (result.equalsIgnoreCase("Monthly")) paymentPeriod = 0;
+        if (result.equalsIgnoreCase("Semi-Monthly")) paymentPeriod = 1;
+
+        if (result.equalsIgnoreCase("Employed")) employment = 0;
+        if (result.equalsIgnoreCase("Self-Employed")) employment = 1;
+        if (result.equalsIgnoreCase("Voluntary")) employment = 1;
+        if (result.equalsIgnoreCase("OFW")) employment = 1;
+
+        if (result.equalsIgnoreCase("Single")) civilStatus = 0;
+        if (result.equalsIgnoreCase("Married")) civilStatus = 1;
+        if (result.equalsIgnoreCase("Single w/1 Dependent")) civilStatus = 2;
+        if (result.equalsIgnoreCase("Single w/2 Dependent")) civilStatus = 3;
+        if (result.equalsIgnoreCase("Single w/3 Dependent")) civilStatus = 4;
+        if (result.equalsIgnoreCase("Single w/4 Dependent")) civilStatus = 5;
+        if (result.equalsIgnoreCase("Married w/1 Dependent")) civilStatus = 6;
+        if (result.equalsIgnoreCase("Married w/2 Dependent")) civilStatus = 7;
+        if (result.equalsIgnoreCase("Married w/3 Dependent")) civilStatus = 8;
+        if (result.equalsIgnoreCase("Married w/4 Dependent")) civilStatus = 9;
+
+        if (salary > 0) setContributions(salary, employment, paymentPeriod);
     }
 
     public void onNothingSelected(AdapterView<?> parent) {
 
     }
+
+    public void setContributions(double salary, int employmentType, int payPeriod){
+
+        ContributionsManager contributionsManager = new ContributionsManager();
+        double sssContribution = contributionsManager.sssContribution(salary, employmentType, payPeriod);
+        double philhealthContribution = contributionsManager.philhealthContribution(salary, payPeriod);
+        double pagibigContribution = contributionsManager.pagIbigContribution(salary);
+
+        sssEditTextView.setText("" + sssContribution);
+        phEditTextView.setText("" + philhealthContribution);
+        pagibigEditTextView.setText("" + pagibigContribution);
+    }
+
 }
